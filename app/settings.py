e9812898ce9b85938efbcede6e6f960ede34b496
p14s <tsangernest@hotmail.com>
2025-11-10 20:27:10 -0600

    * remove timezone * change to correct language code

--- app/settings.py
+++ app/settings.py
@@ -128,13 +128,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en-ca"
 
 TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
