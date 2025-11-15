0ce9cd0ce889ecbb68a2efe652ac28cdeda9bb06
p14s <tsangernest@hotmail.com>
2025-11-14 23:19:11 -0600

    * added test for migration and circuits

--- tests/test_migrations/test_nationality_migration.py
+++ tests/test_migrations/test_nationality_migration.py
@@ -17,11 +17,21 @@ def test_nationality_migration(migrator):
 @pytest.mark.django_db
 def test_normalize_nationality_migration(migrator):
     old_state = migrator.apply_initial_migration(("app", "0005_normalize_nationality"))
+    old_circuit = old_state.apps.get_model("app.Circuit").objects.only("country")
     old_driver = old_state.apps.get_model("app.Driver").objects.only("nationality")
 
     # * For all previous database state drivers, there does not exist a driver
     #    that is assigned with new unique set of Nationality objects.
     #    Designated using the 'deleted_at' attribute
     assert not old_driver.filter(nationality__deleted_at__isnull=True).exists()
+    assert not old_circuit.filter(country__deleted_at__isnull=True).exists()
 
+    ##############
+    # * Post migration *
+    new_state = migrator.apply_tested_migration(("app", "0006_reassign_nationality"))
+    new_circuit = new_state.apps.get_model("app.Circuit").objects.only("country")
+    new_driver = new_state.apps.get_model("app.Driver").objects.only("nationality")
+
+    assert new_circuit.filter(country__deleted_at__isnull=True).exists()
+    assert new_driver.filter(nationality__deleted_at__isnull=True).exists()
 
