8a4e063203e7e374cbdb8e3f01cbe2af9144cde7
p14s <tsangernest@hotmail.com>
2025-11-15 21:04:22 -0600

    * formatting,

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,20 @@
+import pytest
 from pathlib import Path
-from pytest import fixture
 
 from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.test import Client
 
 
-@fixture(scope="session")
+@pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         for f in Path("/app/tests/fixtures/").glob("*.json"):
              call_command("loaddata", f, "--verbosity=3", "--format=json")
 
 
-@fixture
-def c():
+@pytest.fixture
+def django_client():
     u = User.objects.create(
         is_active=True,
         first_name="test_open_first",
