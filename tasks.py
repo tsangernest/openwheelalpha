6aced9d08fc3a13eb23b9dfbf7dd9dd02a5ad26a
p14s <tsangernest@hotmail.com>
2026-02-10 14:58:06 -0600

    * updated image file, updated venv creation

--- tasks.py
+++ tasks.py
@@ -5,10 +5,10 @@ from invoke import task
 def virtualenvironment(c, update=False):
     print(f"\n***\nCreating VirtualEnvironment\n***\n")
 
-    c.run("python3.12 -m venv .venv/", pty=True)
+    c.run("python3.12 -m venv .venv/ --prompt=openwheelalpha", pty=True)
     c.run("source .venv/bin/activate", pty=True)
-    c.run("pip install --U 'pip<25.3'", pty=True)
-    c.run("pip install -U pip-tools setuptools wheel psycopg2-binary", pty=True)
+    c.run("pip install -U 'pip<25.3' --no-cache-dir", pty=True)
+    c.run("pip install -U pip-tools setuptools psycopg2-binary --no-cache-dir", pty=True)
 
     if update:
         c.run("pip-compile requirements.in", pty=True)
